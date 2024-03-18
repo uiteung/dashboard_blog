08aa2140987ee22499387c26ad2d468994e05feb
indrariksa <indrariksa@gmail.com>
2024-03-18 12:32:23 +0700

    fix(dropdown): ubah logika dropdown

--- static/js/controller/dropdown.js
+++ static/js/controller/dropdown.js
@@ -1,28 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Ambil elemen dropdown
-    const prodiDropdown = document.getElementById("prodiDropdown");
+prodiDropdown.addEventListener("change", async function (event) {
+    event.preventDefault(); // Mencegah perilaku bawaan dari event change dropdown
 
-    // Cek apakah ada nilai prodiID yang tersimpan di localStorage
-    const storedProdiID = localStorage.getItem("selectedProdiID");
-
-    // Jika ada, atur nilai dropdown sesuai dengan nilai yang tersimpan
-    if (storedProdiID) {
-        prodiDropdown.value = storedProdiID;
-    }
-
-    // Tambahkan event listener untuk menangani perubahan nilai dropdown
-    prodiDropdown.addEventListener("change", function () {
-        // Dapatkan nilai prodiID yang dipilih
-        const selectedProdiID = prodiDropdown.value;
-
-        // Simpan nilai prodiID ke localStorage
-        localStorage.setItem("selectedProdiID", selectedProdiID);
-
-        // Perbarui URL dengan menambahkan query parameter prodiID
-        const currentURL = new URL(window.location.href);
-        currentURL.searchParams.set("prodiID", selectedProdiID);
-
-        // Redirect ke URL yang diperbarui
-        window.location.href = currentURL.toString();
-    });
+    const selectedProdiID = prodiDropdown.value;
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('prodiID', selectedProdiID);
+    const newUrl = window.location.pathname + '?' + urlParams.toString(); // Dapatkan URL baru
+    history.pushState({}, '', newUrl); // Perbarui URL tanpa menyebabkan pembaruan halaman
 });
