60ba7922506ce5eeeee1c678d0e1de2f3f25191c
indrariksa <indrariksa@gmail.com>
2024-03-18 11:37:17 +0700

    feat(postview): tambah js async await untuk fetch

--- static/js/post_view.js
+++ static/js/post_view.js
@@ -3,8 +3,7 @@ import { CihuyDomReady, CihuyQuerySelector } from "https://c-craftjs.github.io/t
 import { UrlGetAllPost, requestOptionsGet } from "./controller/template.js";
 import { token } from "./controller/cookies.js";
 
-// Untuk Membuat Pagination
-CihuyDomReady(() => {
+CihuyDomReady(async () => {
 	const tablebody = CihuyId("tablebody");
 	const buttonsebelumnya = CihuyId("prevPageBtn");
 	const buttonselanjutnya = CihuyId("nextPageBtn");
@@ -19,103 +18,106 @@ CihuyDomReady(() => {
 	// Use prodiID in your fetch request or wherever needed
 	const fetchUrl = UrlGetAllPost + "?prodiID=" + prodiID;
 
-	fetch(fetchUrl, requestOptionsGet)
-		.then((result) => {
-			return result.json();
-		})
-		.then((data) => {
-			console.log(data);
-			// Check if data.data.items is an array before using map
-			if (Array.isArray(data.data.items)) {
-				let tableData = "";
-				data.data.items.map((values, index) => {
-					const sequentialNumber = index + 1;
+	try {
+		const response = await fetch(fetchUrl, await requestOptionsGet());
 
-					// Format the date
-					const dateObj = new Date(values.updated);
-					const options = {
-						day: 'numeric',
-						month: 'long', // Use 'short' or 'long' for abbreviated or full month names
-						year: 'numeric',
-					};
+		if (!response.ok) {
+			throw new Error('Gagal mendapatkan semua posting');
+		}
 
-					const formattedDate = new Intl.DateTimeFormat('id-ID', options).format(dateObj);
+		const data = await response.json();
+		console.log(data);
+		// Check if data.data.items is an array before using map
+		if (Array.isArray(data.data.items)) {
+			let tableData = "";
+			data.data.items.map((values, index) => {
+				const sequentialNumber = index + 1;
 
-					const timePart = dateObj.toLocaleTimeString('id-ID', {
-						hour12: false
-					});
+				// Format the date
+				const dateObj = new Date(values.updated);
+				const options = {
+					day: 'numeric',
+					month: 'long', // Use 'short' or 'long' for abbreviated or full month names
+					year: 'numeric',
+				};
 
-					tableData += `
-                        <tr>
-                        <td hidden></td>
-						<td style="text-align: center; vertical-align: middle">
-                            <p class="fw-normal mb-1">${sequentialNumber}</p>
-                        </td>
-						<td style="text-align: center; vertical-align: middle">
-							<p class="fw-normal mb-1">${formattedDate.replace('.', ',')}, <br>Pukul ${timePart} WIB</p>
-						</td>                           
-                        <td style="text-align: center; vertical-align: middle">
-                            <p class="fw-normal mb-1">${values.title}</p>
-                        </td>               
-                        <td style="text-align: center; vertical-align: middle">
-                            <a href="${values.url}" target="_blank">${values.url}</a>
-                        </td>                           
-                        <td style="text-align: center; vertical-align: middle">
-                            <p class="fw-normal mb-1">${values.author.displayName}</p>
-                        </td>               
-                        <td style="text-align: center; vertical-align: middle">
-                            <p class="fw-normal mb-1">${values.replies.totalItems}</p>
-                        </td>               
-						<td style="text-align: center; vertical-align: middle">
-                            <p class="fw-normal mb-1">${values.id}</p>
-                        </td> 
-                    </tr>`;
+				const formattedDate = new Intl.DateTimeFormat('id-ID', options).format(dateObj);
+
+				const timePart = dateObj.toLocaleTimeString('id-ID', {
+					hour12: false
 				});
-				document.getElementById("tablebody").innerHTML = tableData;
 
-				displayData(halamannow);
-				updatePagination();
-			} else {
-				console.error('Items is not an array:', data.data.items);
-			}
-		})
-		.catch(error => {
-			console.log('error', error);
-		});
+				tableData += `
+                    <tr>
+                    <td hidden></td>
+                    <td style="text-align: center; vertical-align: middle">
+                        <p class="fw-normal mb-1">${sequentialNumber}</p>
+                    </td>
+                    <td style="text-align: center; vertical-align: middle">
+                        <p class="fw-normal mb-1">${formattedDate.replace('.', ',')}, <br>Pukul ${timePart} WIB</p>
+                    </td>                           
+                    <td style="text-align: center; vertical-align: middle">
+                        <p class="fw-normal mb-1">${values.title}</p>
+                    </td>               
+                    <td style="text-align: center; vertical-align: middle">
+                        <a href="${values.url}" target="_blank">${values.url}</a>
+                    </td>                           
+                    <td style="text-align: center; vertical-align: middle">
+                        <p class="fw-normal mb-1">${values.author.displayName}</p>
+                    </td>               
+                    <td style="text-align: center; vertical-align: middle">
+                        <p class="fw-normal mb-1">${values.replies.totalItems}</p>
+                    </td>               
+                    <td style="text-align: center; vertical-align: middle">
+                        <p class="fw-normal mb-1">${values.id}</p>
+                    </td> 
+                </tr>`;
+			});
+			document.getElementById("tablebody").innerHTML = tableData;
 
-function displayData(page) {
-	const baris = CihuyQuerySelector("#tablebody tr");
-	const mulaiindex = (page - 1) * itemperpage;
-	const akhirindex = mulaiindex + itemperpage;
-
-	for (let i = 0; i < baris.length; i++) {
-		if (i >= mulaiindex && i < akhirindex) {
-			baris[i].style.display = "table-row";
+			displayData(halamannow);
+			updatePagination();
 		} else {
-			baris[i].style.display = "none";
+			console.error('Items is not an array:', data.data.items);
+		}
+	} catch (error) {
+		console.error('Terjadi kesalahan:', error);
+	}
+
+	function displayData(page) {
+		const baris = CihuyQuerySelector("#tablebody tr");
+		const mulaiindex = (page - 1) * itemperpage;
+		const akhirindex = mulaiindex + itemperpage;
+
+		for (let i = 0; i < baris.length; i++) {
+			if (i >= mulaiindex && i < akhirindex) {
+				baris[i].style.display = "table-row";
+			} else {
+				baris[i].style.display = "none";
+			}
 		}
 	}
-}
-function updatePagination() {
-	halamansaatini.textContent = `Halaman ${halamannow}`;
-}
 
-buttonsebelumnya.addEventListener("click", () => {
-	if (halamannow > 1) {
-		halamannow--;
-		displayData(halamannow);
-		updatePagination();
+	function updatePagination() {
+		halamansaatini.textContent = `Halaman ${halamannow}`;
 	}
-});
 
-buttonselanjutnya.addEventListener("click", () => {
-	const totalPages = Math.ceil(
-		tablebody.querySelectorAll("#tablebody tr").length / itemperpage
-	);
-	if (halamannow < totalPages) {
-		halamannow++;
-		displayData(halamannow);
-		updatePagination();
-	}
-  });
+	buttonsebelumnya.addEventListener("click", () => {
+		if (halamannow > 1) {
+			halamannow--;
+			displayData(halamannow);
+			updatePagination();
+		}
+	});
+
+	buttonselanjutnya.addEventListener("click", () => {
+		const totalPages = Math.ceil(
+			tablebody.querySelectorAll("#tablebody tr").length / itemperpage
+		);
+		if (halamannow < totalPages) {
+			halamannow++;
+			displayData(halamannow);
+			updatePagination();
+		}
+	});
 });
