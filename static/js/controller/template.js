07bf2cfc7cb14e3b7f6deee4e58667972c9d49c4
indrariksa <indrariksa@gmail.com>
2024-03-18 11:36:58 +0700

    feat(template): tambah js async await req header

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -1,16 +1,49 @@
 // Import Libray yg dibutuhkan
 import { token } from "./cookies.js";
+import { getApiKey } from "./key.js";
 
-// header
-var header = new Headers();
-header.append("login", token);
-header.append("apiKey", "AIzaSyA9lK3ZvoSraJ2mdL8ftOGsEjut6EngZ7s");
-header.append("Content-Type", "application/json");
+// // header
+// var header = new Headers();
+// header.append("login", token);
+// header.append("apiKey", "AIzaSyA9lK3ZvoSraJ2mdL8ftOGsEjut6EngZ7s");
+// header.append("Content-Type", "application/json");
+
+// // Membuat objek konfigurasi untuk permintaan GET
+// export let requestOptionsGet = {
+//   method: 'GET',
+//   headers: header
+// }
+
+async function setHeaders() {
+  try {
+    const apiKey = await getApiKey();
+
+    // Membuat objek header
+    var header = new Headers();
+    header.append("login", token);
+    header.append("apiKey", apiKey);
+    header.append("Content-Type", "application/json");
+
+    return header;
+  } catch (error) {
+    console.error('Terjadi kesalahan saat mengatur header:', error);
+    throw error;
+  }
+}
 
 // Membuat objek konfigurasi untuk permintaan GET
-export let requestOptionsGet = {
-  method: 'GET',
-  headers: header
+export async function requestOptionsGet() {
+  try {
+    const header = await setHeaders();
+
+    return {
+      method: 'GET',
+      headers: header
+    };
+  } catch (error) {
+    console.error('Terjadi kesalahan saat membuat objek konfigurasi permintaan:', error);
+    throw error;
+  }
 }
 
 // Endpoint Get All
